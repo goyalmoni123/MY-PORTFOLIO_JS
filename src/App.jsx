8ad04b10a60660cd91d58e7370a263e060a53b6e
goyalmoni123 <157277144+goyalmoni123@users.noreply.github.com>
2024-07-03 19:33:32 +0000

    committinggg

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Monika Goyal",
+  title: "Frontend Developer",
+  email: "goyalmoni9210@gmail.com",
+  gitHub: "goyalmoni123",
+  instagram: "",
+  linkedIn: "Monika goyal",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
